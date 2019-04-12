72576c061a599d9c4be33147b6aea92e29845f72
kazukis2 <kazukis2@illinois.edu>
2019-04-11 23:02:01 -0500

    typed up all of part 2, debugging phase

--- MP4 Reinforcement Learning/part 2/neural_network.py
+++ MP4 Reinforcement Learning/part 2/neural_network.py
@@ -1,4 +1,12 @@
 import numpy as np
+import random
+
+def shuffle(x_train,y_train):
+    seed = random.randint(1,101)
+    np.random.seed(seed)
+    np.random.shuffle(x_train)
+    np.random.shuffle(y_train)
+    return x_train, y_train
 
 """
     Minigratch Gradient Descent Function to train model
@@ -22,9 +30,26 @@ import numpy as np
         (Stdout print will not affect autograder as long as runtime is within limits)
 """
 def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_classes, shuffle=True):
+    # 1. Format the data
+    num_examples = len(x_train)
+    batch_size = 200
+    full_set = (int) (num_examples / batch_size)
+    losses = np.zeros(epoch)
+    # 2. call four_nn function to obtain losses
+    for e in range(epoch):
+        print("Epoch #: "+str(e))
+        # shuffle data
+        x_temp, y_temp = x_train, y_train
+        if shuffle:
+            x_temp, y_temp = shuffle(x_train, y_train)
 
-    #IMPLEMENT HERE
-
+        total_loss = 0
+        for i in range(full_set):
+            X, y = x_temp[i * batch_size: i * batch_size + batch_size], y_temp[i * batch_size: i * batch_size + batch_size]
+            loss, w1, w2, w3, w4 = four_nn(X, w1, w2, w3, w4, b1, b2, b3, b4, y, False)
+            total_loss += loss
+        losses[e] = loss
+    # 3. Return all the weights/biases and a list of losses at each epoch
     return w1, w2, w3, w4, b1, b2, b3, b4, losses
 
 """
@@ -45,8 +70,21 @@ def minibatch_gd(epoch, w1, w2, w3, w4, b1, b2, b3, b4, x_train, y_train, num_cl
 """
 def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
 
-    avg_class_rate = 0.0
+    num_examples = len(x_train)
+    batch_size = 200
+    full_set = (int) (num_examples / batch_size)
+    corrects = 0
     class_rate_per_class = [0.0] * num_classes
+    avg_class_rate = 0
+
+    for i in range(len(x_test)):
+        for i in range(full_set):
+            X, y = x_temp[i * batch_size: i * batch_size + batch_size], y_temp[i * batch_size: i * batch_size + batch_size]
+            res_max = four_nn(X, w1, w2, w3, w4, b1, b2, b3, b4, y, True)
+            if res_max == y_test[i]:
+                avg_class_rate += 1
+
+    avg_class_rate /= num_classes
     return avg_class_rate, class_rate_per_class
 
 """
@@ -55,8 +93,37 @@ def test_nn(w1, w2, w3, w4, b1, b2, b3, b4, x_test, y_test, num_classes):
     Up to you on how to implement this, won't be unit tested
     Should call helper functions below
 """
-def four_nn():
-    pass
+def four_nn(X, w1, w2, w3, w4, b1, b2, b3, b4, y,test):
+    learning_rate = 0.1
+
+    Z1, acache1 = affine_forward(X,w1,b1)
+    A1, rcache1 = relu_forward(Z1)
+    Z2, acache2 = affine_forward(A1,w2,b2)
+    A2, rcache2 = relu_forward(Z2)
+    Z3, acache3 = affine_forward(A2,w3,b3)
+    A3, rcache3 = relu_forward(Z3)
+    F, acache4 = affine_forward(A3,w4,b4)
+
+    # return classifications = argmax over all classes in logits for each example
+    if test:
+        return [np.argmax(x) for x in F]
+
+    loss, dF = cross_entropy(F,y)
+    dA3, dW4, db4 = affine_backward(dF, acache4)
+    dZ3 = relu_backward(dA3, rcache3)
+    dA2, dW3, db3 = affine_backward(dZ3,acache3)
+    dZ2 = relu_backward(dA2, rcache2)
+    dA1, dW2, db2 = affine_backward(dZ2,acache2)
+    dZ1 = relu_backward(dA1, rcache1)
+    dX, dW1, db1 = affine_backward(dZ1,acache1)
+
+    # use gradient descent to update parameters W1 = w1 - n dW1
+    w1 -= learning_rate * dW1
+    w2 -= learning_rate * dW2
+    w3 -= learning_rate * dW3
+    w4 -= learning_rate * dW4
+
+    return loss, w1, w2, w3, w4
 
 """
     Next five functions will be used in four_nn() as helper functions.
@@ -68,16 +135,51 @@ def four_nn():
         This is a great time to review on your linear algebra as well.
 """
 def affine_forward(A, W, b):
+    Z = np.matmul(A, W) + b
+    cache = (A, W, b)
     return Z, cache
 
 def affine_backward(dZ, cache):
+    A = cache[0]
+    W = cache[1]
+    b = cache[2]
+    dA = np.matmul(dZ, W.T)
+    dW = np.matmul(A.T, dZ)
+    dB = sum(dZ)
     return dA, dW, dB
 
 def relu_forward(Z):
+    A = Z.copy()
+    cache = Z
+    A[A <= 0] = 0
     return A, cache
 
 def relu_backward(dA, cache):
-    return dA
+    Z = cache.copy()
+    dZ = dA.copy()
+    dZ[Z <= 0] = 0
+    return dZ
+
 
 def cross_entropy(F, y):
+    loss = 0
+    fy_total = 0
+    f_total = 0
+    dF = F.copy()
+    for i in range(F.shape[0]):
+        y_i = y[i]
+        fy_total += F[i, y_i]
+        for k in range(F.shape[1]):
+            f_total += np.exp(F[i, k])
+        fy_total -= np.log(f_total)
+    loss = -1 * fy_total / F.shape[0]
+
+    for i in range(F.shape[0]):
+        for j in range(F.shape[1]):
+            sum_exp = 0
+            for k in range(F.shape[1]):
+                sum_exp += np.exp(F[i, k])
+            binary = j == y[i]
+            dF[i, j] = binary - np.exp(F[i, j]) / sum_exp
+            dF[i, j] /= (-1 * F.shape[0])
     return loss, dF
